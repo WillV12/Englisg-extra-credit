97b6aa2221a64c42f5415fb39088a3b030c5fe89
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-02-16 17:03:31 -0500

    Updates main
    title card and color, added loading dots to each game mode, comments for readability

--- main.py
+++ main.py
@@ -6,9 +6,7 @@
 from time import sleep
 from colorama import Fore, Back, Style
 DOTS = "....."
-
-def main():
-    title_picture = '''     )                  )      )   (                     )      
+TITLE_PICTURE = '''     )                  )      )   (                     )      
   ( /(   (       (   ( /(   ( /(   )\\ )        *   )  ( /(      
   )\\())  )\\    ( )\\  )\\())  )\\()) (()/( (    ` )  /(  )\\()) (   
 |((_)\\((((_)(  )((_)((_)\\  ((_)\\   /(_)))\\    ( )(_))((_)\\  )\\  
@@ -17,12 +15,24 @@ def main():
 | ' <   / _ \\  | _ \\| (_) || (_) |\\__ \\| _|  _ | |   | __ || _| 
 |_|\\_\\ /_/ \\_\\ |___/ \\___/  \\___/ |___/|___|(_)|_|   |_||_||___|'''
 
-    for line in title_picture.splitlines():
+
+def main():
+    # Prints the Title card
+    for line in TITLE_PICTURE.splitlines():
         print(f"{Fore.LIGHTYELLOW_EX}{Back.LIGHTRED_EX}{Style.BRIGHT}{line}{Style.RESET_ALL}")
         sleep(.094)
 
-    print("Welcome to Kaboose.the!\nThe rules are simple...\n\tEnter the correct answer (A, B, C, or D)")
+    # Greeting/Game Rules/Getting player name
+    print(f"Welcome to Kaboose.the!\n\
+    The rules are simple...\
+    \n\tEnter the correct answer (A, B, C, or D)\n")
 
+    player_name = input("Enter your Nickname (or real it don't matter):\n\t")
+
+    print(f"Get ready {player_name}, things are about to get a little bit \
+    {Fore.LIGHTYELLOW_EX}{Back.LIGHTRED_EX}{Style.BRIGHT}WICKED{Style.RESET_ALL}\n\n")
+
+    #EASY MODE
     print("Beginning Wave 1 (easy mode)")
 
     for char in DOTS:
@@ -30,12 +40,14 @@ def main():
         sleep(.34)
     print()
 
+    # MEDIUM MODE
     print("Beginning Wave 2 (intermidiate mode)")
     for char in DOTS:
         print(char + "\t", end="")
         sleep(.34)
     print()
 
+    # AH IT'S SO HARD... SO HARD... IT'S SO HARD
     print("Beginning Wave 3 (HARD mode)")
     for char in DOTS:
         print(char + "\t", end="")
