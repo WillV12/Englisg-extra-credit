281995ced13e9fb9a6a6779a6b398fc0578c5854
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-02-16 22:39:04 -0500

    dgfdgf

--- test world.py
+++ test world.py
@@ -4,22 +4,24 @@ from time import sleep
 stuff = {"apple": "red", "pear": "green", "banana": "yellow", "orange": "orange", "grape": "purple"}
 questions = ["apple", "pear", "banana", "orange", "grape"]
 letters = ["A.", "B.", "C.", "D."]
-count = 3
+count = 4
 
 while len(questions) > 0:
-    stuff_keys = ["apple", "pear", "banana", "orange", "grape"]
-    print(stuff_keys)
     index = r.randint(0, count)
-    print(questions[index])
+    print(stuff[questions[index]])
     count -= 1
     questions.remove(questions[index])
+    other_count = 3
+    answers = []
     for x in letters:
-        other_count = 3
         other_index = r.randint(0, other_count)
         other_count -= 1
-        print(f"{x} {stuff[stuff_keys[other_index]]}")
-        stuff_keys.remove(stuff_keys[other_index])
+        print(f"{x} {questions[other_index]}")
+        questions.remove(questions[other_index])
         sleep(.5)
+
+    # correct_answer = r.randint(4)
+
     break
 
 # print(f"A. {stuff[stuff_keys[r.randint(0,3)]]}")
