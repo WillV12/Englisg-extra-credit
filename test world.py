5229d1bd9e623f60cf38e6ec3b946193d5742ad0
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2024-02-16 22:57:51 -0500

    Create test world.py

---
+++ test world.py
@@ -0,0 +1,18 @@
+import random as r
+from time import sleep
+
+
+questions = {"red", "green", "yellow", "orange", "purple"}
+answers = ["apple", "pear", "banana", "orange", "grape"]
+letters = ["A.", "B.", "C.", "D."]
+count = 3
+
+
+while len(questions) > 0:
+    rand_index = r.randint(0,5)
+    correct_pos = r.randint(0,4)
+    print(questions[rand_index])
+    
+    for x in letters:
+        if 
+
