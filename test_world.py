dd6d362e85220d386b3fada87bf40dbd18be63bb
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-02-16 23:28:10 -0500

    Update test_world.py

--- test_world.py
+++ test_world.py
@@ -3,17 +3,26 @@ from time import sleep
 
 
 questions = ["red", "green", "yellow", "orange", "purple"]
-answers = ["apple", "pear", "banana", "orange", "grape"]
 letters = ["A.", "B.", "C.", "D."]
 
 
 while len(questions) > 0:
-    rand_index = r.randint(0,5)
-    correct_pos = r.randint(0,4)
+    answers = ["apple", "pear", "banana", "orange", "grape"]
+    rand_index = r.randint(0,4)
+    correct_pos = r.randint(0,3)
+    correct_ans = answers[rand_index]
+    answers.remove(correct_ans)
     print(questions[rand_index])
-    
+    count = 0
+    index_limit = 3
     for x in letters:
-        
-        if 
+        other_ans = r.randint(0,index_limit)
+        if count == correct_pos:
+            print(f"{x} {correct_ans}")
         else:
-            print(f"")
+            print(f"{x} {answers[other_ans]}")
+            answers.remove(answers[other_ans])
+            index_limit -= 1
+        count += 1
+        sleep(.5)
+    break
