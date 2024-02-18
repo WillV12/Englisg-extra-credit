626533727e11d3c0f055497ec177300f757a24fb
Will Vanderploeg <thatmurphyboi@gmail.com>
2024-02-17 23:40:58 -0500

    In the club straight up jorkin it
    And by it, well lets just say haha my penis

--- game.py
+++ game.py
@@ -5,14 +5,6 @@
 import random as r
 from time import sleep
 
-<<<<<<< Updated upstream
-def question_format(questions, answers):
-    letters = ["A.", "B.", "C.", "D."]
-
-    while len(questions) > 0:
-        # questions_changed = questions[:]
-        rand_index = r.randint(0,4)
-=======
 
 def question_format(questions, answers, mode):
     letters = ["A", "B", "C", "D"]
@@ -25,49 +17,18 @@ def question_format(questions, answers, mode):
         answer_indexs = []
         correct_ans = r.choice(answers)
         correct_index = answers.index(correct_ans)
->>>>>>> Stashed changes
         correct_pos = r.randint(0,3)
-        correct_ans = answers[rand_index]
         answers.remove(correct_ans)
-<<<<<<< Updated upstream
-        print(questions[rand_index])
-        count = 0
-        index_limit = len(answers) - 1
-=======
         print(f"{mode} Q{Q_counter}: {questions[correct_index]}")
         questions.pop(correct_index)
         count = 0
         print("--------------------------------------------------")
         count2 = 0
         Q_counter += 1
->>>>>>> Stashed changes
         for x in letters:
-            other_ans = r.randint(0, index_limit)
             if count == correct_pos:
                 print(f"{x} {correct_ans}")
             else:
-<<<<<<< Updated upstream
-                print(f"{x} {answers[other_ans]}")
-                answers.remove(answers[other_ans])
-                index_limit -= 1
-            count += 1
-            sleep(.5)
-        input_and_checking(letters[correct_pos])
-
-
-def input_and_checking(answer):
-    user_answer = input("Enter answer (A, B, C, or D): ")
-    user_answer = user_answer.upper()
-    sleep(1)
-    if user_answer != ('A' or 'B' or 'C' or 'D'):
-        if user_answer == answer:
-            print("Correct!!")
-        else:
-            print("Incorrect")
-    else:
-        input_and_checking(answer)
-<<<<<<< Updated upstream
-=======
                 other_ans = r.choice(answer_choices)
                 while other_ans in answer_indexs:
                     other_ans = r.choice(answer_choices)
@@ -102,6 +63,3 @@ def input_and_checking(letters):
 
     return user_answer
 
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
